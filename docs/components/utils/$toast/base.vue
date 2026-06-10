8db3a7afbeb54f07684141e2e5c3e753b4120fef
liulihao <551772063@qq.com>
2026-06-10 10:44:31 +0800

    fix: a

--- docs/components/utils/$toast/base.vue
+++ docs/components/utils/$toast/base.vue
@@ -1,15 +1,13 @@
 <script setup lang="ts">
 import { ref, getCurrentInstance } from 'vue'
-import { $toast, sleep } from '@sybz-components/utils'
+import { $toast } from '@sybz-components/utils'
 const isTest52 = async () => {
   $toast('测试成功')
-  await sleep(300)
-  $toast('测试成功2')
 }
 </script>
 
 <template>
   <div>
-    <el-button type="primary" @click="isTest52">测试11</el-button>
+    <el-button type="primary" @click="isTest52">测试</el-button>
   </div>
 </template>
