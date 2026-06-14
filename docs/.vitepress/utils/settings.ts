5eab2f6f83e0539294119e548b2cf5bed3259232
liulihao <551772063@qq.com>
2026-06-14 15:20:21 +0800

    fix: 再试试搜索

--- docs/.vitepress/utils/settings.ts
+++ docs/.vitepress/utils/settings.ts
@@ -1,6 +1,6 @@
 export function createAlgolia() {
   return {
-    appId: '0WC5FF7N30',
+    appId: 'OWC5FF7N30',
     apiKey: '610e973b9963174fcbc3409dfca4bacb',
     indexName: 'liulihao88_github_io_owc5ff7n30_pages',
     placeholder: '请输入关键词',
