bf6afbb9a90bae134845875b6a834a80c623eee2
liulihao <551772063@qq.com>
2026-06-14 15:02:50 +0800

    fix: 更新

--- docs/.vitepress/utils/settings.ts
+++ docs/.vitepress/utils/settings.ts
@@ -1,10 +1,8 @@
 export function createAlgolia() {
   return {
-    // 如果 Algolia 没有为你提供 `appId` ，使用 `BH4D9OD16A` 或者移除该配置项
-    appId: 'ZN8VN43G8D',
-    apiKey: '6b6e7845261ceff59ea5eeb84e0bde86',
-    indexName: 'sybz-components',
-    container: '### REPLACE ME WITH A CONTAINER (e.g. div) ###',
+    appId: '0WC5FF7N30',
+    apiKey: '610e973b9963174fcbc3409dfca4bacb',
+    indexName: 'liulihao88_github_io_owc5ff7n30_pages',
     placeholder: '请输入关键词',
     translations: {
       button: {
