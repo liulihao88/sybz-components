5a339875b8eac8b0f74cc311cfbb2b54c7c652c4
liulihao <551772063@qq.com>
2026-06-17 10:57:12 +0800

    fix: a

--- docs/components/dateRange/base.vue
+++ docs/components/dateRange/base.vue
@@ -1,26 +1,12 @@
 <script setup lang="ts">
 import { ref, getCurrentInstance, h } from 'vue'
 const { proxy } = getCurrentInstance()
-const dateValue = ref()
-
-const customPrefix = {
-  render() {
-    return h('p')
-  },
-}
+const dateValue = ref([])
 </script>
 
 <template>
   <div>
     <s-date-range v-model="dateValue"></s-date-range>
-    <s-date-range
-      v-model="dateValue"
-      width="600"
-      size="small"
-      title="选择日期"
-      class="m-t-8"
-      :boxStyle="{ width: 100 }"
-    ></s-date-range>
   </div>
 </template>
 
