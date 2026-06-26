5f495cf903d192a79ab4c8aef2acaa15f52a2ece
liulihao <551772063@qq.com>
2026-06-26 13:46:58 +0800

    fix: a

--- docs/components/test/base.vue
+++ docs/components/test/base.vue
@@ -15,7 +15,7 @@ const c = ref()
     <el-button type="primary" size="small">测试02</el-button>
     <s-button theme="chenghua" size="small" type="primary"></s-button>
     <!-- <s-table></s-table> -->
-    <s-empty description="暂无数据" width="48" height="48"></s-empty>
+    <s-empty description="暂无数据" width="48" height="48"> </s-empty>
 
     <el-dialog title="33"> </el-dialog>
     <s-dialog
