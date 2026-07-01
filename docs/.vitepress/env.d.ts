952a860b1927c900d6f02237ccc127e51ff0dc1c
liulihao <551772063@qq.com>
2026-07-01 13:00:22 +0800

    fix: 更新theme.ts的引入警告

--- docs/.vitepress/env.d.ts
+++ docs/.vitepress/env.d.ts
@@ -1,4 +1,10 @@
 /// <reference path="../../packages/components.d.ts" />
 /// <reference types="vite/client" />
 
-export {}
+declare module '~dist/sybz-components-es.js' {
+  const SybzComponents: import('vue').Plugin
+  function createSvg(iconDirs: string[]): Record<string, any>
+
+  export { createSvg }
+  export default SybzComponents
+}
