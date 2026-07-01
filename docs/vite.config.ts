2a3c2ed043555ade5dc7f0c186de03638009fdad
liulihao <551772063@qq.com>
2026-07-01 13:10:35 +0800

    fix: a

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -51,7 +51,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('../packages', import.meta.url)),
-      '@sybz-components/utils': fileURLToPath(new URL('../packages/utils/src/index.ts', import.meta.url)),
+      // '@sybz-components/utils': fileURLToPath(new URL('../packages/utils/src/index.ts', import.meta.url)),
       '~dist': fileURLToPath(new URL('../dist', import.meta.url)),
     },
     // 类型： string[] 导入时想要省略的扩展名列表。
