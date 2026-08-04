967a6f1e2f16782e5c805e0d00c61aa38368a50c
liulihao <551772063@qq.com>
2026-08-04 11:31:28 +0800

    fix: a

--- docs/components/card/callapse.vue
+++ docs/components/card/callapse.vue
@@ -10,10 +10,10 @@ const isCollapsed = ref(true)
       <template #header>
         <s-title title="默认点击右侧图标收起">
           <template #append>
-            <el-button type="primary" @click.stop="console.log(1234)">测试96</el-button>
+            <el-button type="primary" size="small" @click.stop="console.log(1234)">测试96</el-button>
           </template>
           <template #extra>
-            <el-button type="primary" @click.stop="console.log(123)">右侧按钮不再需要手动 stop</el-button>
+            <el-button type="primary" size="small" @click.stop="console.log(123)">右侧按钮不再需要手动 stop</el-button>
           </template>
         </s-title>
       </template>
