b4ba0cf0aee580eaf98aeef3fb9c407ef0442b6f
liulihao <551772063@qq.com>
2026-08-06 17:57:18 +0800

    fix: a

--- docs/components/input/other.vue
+++ docs/components/input/other.vue
@@ -8,84 +8,12 @@
       placeholder="Please input password"
       show-password
     />
-
     <s-input v-model="input" width="240" height="48" title="height: 48" placeholder="height: 48" />
-
-    <s-title title="文本域">
-      <template #append>
-        <s-input
-          v-model="textarea"
-          title="文本域"
-          style="width: 240px"
-          :rows="2"
-          type="textarea"
-          placeholder="Please input"
-        />
-      </template>
-    </s-title>
-
-    <s-input v-model="textarea1" style="width: 240px" autosize type="textarea" placeholder="Please input" />
-    <div style="margin: 20px 0" />
-    <s-input
-      v-model="textarea2"
-      style="width: 240px"
-      :autosize="{ minRows: 2, maxRows: 4 }"
-      type="textarea"
-      placeholder="Please input"
-    />
-
-    <s-title title="输入长度限制" :sub-title="subTitle"></s-title>
-
-    <el-input
-      v-model="text"
-      style="width: 240px"
-      maxlength="10"
-      placeholder="Please input"
-      show-word-limit
-      type="text"
-    />
-    <el-input
-      v-model="text"
-      class="m-l-16"
-      style="width: 240px"
-      maxlength="10"
-      placeholder="Please input"
-      show-word-limit
-      word-limit-position="outside"
-      type="text"
-    />
-    <div style="margin: 20px 0" />
-    <el-input
-      v-model="textarea"
-      maxlength="30"
-      style="width: 240px"
-      placeholder="Please input"
-      show-word-limit
-      type="textarea"
-    />
-    <el-input
-      v-model="textarea"
-      class="m-l-16"
-      maxlength="30"
-      style="width: 240px"
-      placeholder="Please input"
-      show-word-limit
-      word-limit-position="outside"
-      type="textarea"
-    />
   </s-flex>
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue'
 
-const textarea1 = ref('')
-const textarea2 = ref('')
-
 const input = ref('')
-
-const text = ref('')
-const textarea = ref('')
-const subTitle =
-  '使用 maxlength 和 minlength 属性, 来控制输入内容的最大字数和最小字数。 "字符数"使用JavaScript字符串长度来衡量。 为文本或文本输入类型设置 maxlength prop可以限制输入值的长度。 允许你通过设置 show-word-limit 到 true 来显示剩余字数。 从 2.11.5 版本开始，你可以将 word-limit-position 设置为 outside，以在输入框外显示字数统计。'
 </script>
