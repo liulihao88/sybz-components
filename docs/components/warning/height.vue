8014c416bcf383c5456c1cb31e162a8b1e698d40
liulihao <551772063@qq.com>
2026-08-13 10:24:26 +0800

    fix: warning高度

--- docs/components/warning/height.vue
+++ docs/components/warning/height.vue
@@ -4,8 +4,9 @@
     <s-warning title="content内容" height="100" />
     <s-warning content="title内容" height="100" />
 
-    <div>
-      <s-warning content="content内容" title="title内容" />
+    <div class="w-100% h-100">
+      <s-warning content="content内容" title="title内容" height="100%" />
     </div>
+    <s-warning height="20" content="高度20"></s-warning>
   </s-flex>
 </template>
