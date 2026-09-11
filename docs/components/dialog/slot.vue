8781b9a132ffff942f8cd124d8f62cfcc63fc39c
liulihao <551772063@qq.com>
2026-09-11 14:51:08 +0800

    fix: a

--- docs/components/dialog/slot.vue
+++ docs/components/dialog/slot.vue
@@ -6,8 +6,14 @@ const isShow = ref(false)
 <template>
   <div>
     <el-button type="primary" @click="isShow = true">显示dialog</el-button>
-    <s-dialog v-model="isShow" title="基础dialog">
-      <template #header>这是header插槽, 默认icon还会保留</template>
+    <s-dialog v-model="isShow" title="基础dialog" theme="shijingshan" sub-title="子">
+      <template #header>
+        <s-flex>
+          这是header插槽, 默认icon还会保留
+          <s-button icon="plus" type="danger" theme="shijingshan" @click="console.log(123)">添加</s-button>
+        </s-flex>
+      </template>
+
       我是内容
 
       <template #footer>这是footer插槽</template>
