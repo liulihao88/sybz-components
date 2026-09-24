85ac8b88e0fb9325b5192cac093db1a4d88454a1
liulihao <551772063@qq.com>
2026-09-24 15:08:59 +0800

    fix: a

--- docs/components/select/quick.vue
+++ docs/components/select/quick.vue
@@ -1,4 +1,4 @@
 <template>
-  <s-select :options="[]" title="showQuick=true（默认值），空选项仍显示按钮" />
+  <s-select :options="[]" title="showQuick=true（默认值)" class="m-r-16" />
   <s-select :options="[]" :show-quick="false" title="showQuick=false，隐藏按钮" />
 </template>
